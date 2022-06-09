5e59caeb3d6860e8eb6f7bd3aa5d4026794679bc
codeinchicago <104577414+codeinchicago@users.noreply.github.com>
2022-06-09 09:18:57 -0500

    Add files via upload

---
+++ models.py
@@ -0,0 +1,38 @@
+
+from app import db
+from datetime import datetime
+from werkzeug.security import generate_password_hash
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    posts = db.relationship('Post', backref='author')
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.password = generate_password_hash(kwargs['password'])
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f"<User|{self.username}>"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(25), nullable=False)
+    body = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # FOREIGN KEY(user_id) REFERENCES user(id)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f"<Post|{self.title}>"
